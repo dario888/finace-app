7c73e97572f17b960bd7be22a904471e1c0e982c
dario888 <dario.bario87@hotmail.com>
2020-08-28 17:28:10 +0200

    Salary 1

--- client/src/components/budget-salary/Budget.tsx
+++ client/src/components/budget-salary/Budget.tsx
@@ -7,23 +7,23 @@ import {v4} from 'uuid';
 export type ExpenseOBJ = {
     id: string,
     expensesName: string,
-    amount: string
+    amount: number
 }
 
 
 const Budget = () => {
 
     const [expenses, setExpenses] = useState<ExpenseOBJ[]>(JSON.parse(localStorage.getItem('expenses') as string ) || [] )
-    const [budget, setBudget] = useState('')
+    const [budget, setBudget] = useState(0)
     const [displayBudget, setDisplayBudget] = useState(0)
-    const [expensesName, setExpensesName] = useState('')
-    const [amount, setAmount] = useState('')
+    const [expensesName, setExpensesName] = useState('') 
+    const [amount, setAmount] = useState(0)
     const [sum, setSum] = useState(0)
     const [edit, setEdit] = useState(false)
     const [id, setId] = useState('')
 
     useEffect(() => {
-        setSum(expenses.reduce((acc, cv) => acc + parseInt(cv.amount), 0))
+        setSum(expenses.reduce((acc, cv) => acc + cv.amount, 0))
         localStorage.setItem('expenses', JSON.stringify(expenses))
 
     }, [expenses])
@@ -32,17 +32,17 @@ const Budget = () => {
     // console.log(id )
 
   
-    const setBudgetHendler = (e: ChangeEvent<HTMLInputElement>) => setBudget(e.target.value)
+    const setBudgetHendler = (e: ChangeEvent<HTMLInputElement>) => setBudget(parseInt(e.target.value))
     
     const submitBudget = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        setDisplayBudget(parseInt(budget))
-        setBudget('')
+        setDisplayBudget(budget)
+        setBudget(0)
     }
 
     const setExpensesHendler = (e: ChangeEvent<HTMLInputElement>) => setExpensesName(e.target.value)
 
-    const setAmountHendler = (e: ChangeEvent<HTMLInputElement>) => setAmount(e.target.value)
+    const setAmountHendler = (e: ChangeEvent<HTMLInputElement>) => setAmount(parseInt(e.target.value))
 
     const submitExpenses = (e: FormEvent<HTMLFormElement>) => { 
         e.preventDefault()
@@ -53,7 +53,7 @@ const Budget = () => {
 
             setEdit(false) 
             setExpensesName('')
-            setAmount('')
+            setAmount(0)
 
             return setExpenses(editExpense)
 
@@ -62,7 +62,7 @@ const Budget = () => {
         setExpenses([...expenses, {id: v4(), expensesName, amount}])
 
         setExpensesName('')
-        setAmount('')
+        setAmount(0)
     }
 
     const deleteHendler = (Id: string) => {
@@ -83,7 +83,7 @@ const Budget = () => {
     const editCancle = () => {
         setEdit(false);
         setExpensesName('')
-        setAmount('')
+        setAmount(0)
 
     }
 
@@ -101,7 +101,7 @@ const Budget = () => {
                 <form className="budgetForm" onSubmit={submitBudget}>
                     <h3>Budget</h3>
                     <div className="formGroup">
-                        <input type="number" value={budget} onChange={setBudgetHendler}
+                        <input type="number" value={budget.toString()} onChange={setBudgetHendler}
                         className="inputForms" id="bgtInp" required/>
                     </div>
                     <button type="submit" className="btnForm" id="budgetSubmit">
@@ -116,7 +116,7 @@ const Budget = () => {
                     </div>
                     <h3>Amount</h3>
                     <div className="formGroup">
-                        <input type="number" value={amount} onChange={setAmountHendler}
+                        <input type="number" value={amount.toString()} onChange={setAmountHendler}
                         className="inputForms" id="amtInp" required/>
                     </div>
                     <button type="button" onClick={editCancle} className="btnForm " id="expenseSubmit">
--- client/src/components/budget-salary/Salary.tsx
+++ client/src/components/budget-salary/Salary.tsx
@@ -1,72 +1,124 @@
-import React from 'react'
+import React, {useState, useEffect, ChangeEvent} from 'react'
+
+
+
+
 
 const Salary = () => {
+
+    let [grossSalary, setGrossSalary] = useState(0)
+    const [netSalary, setNetSalary] = useState(0)
+
+    const grossMin = 20335;
+    const netMin = 14000;
+    
+    const pensionFund = 0.188;
+    const healthcareIns = 0.075;
+    const employmentContributions = 0.012;
+    const injuryInsurance = 0.005;
+    const totalContributions = Math.round(grossSalary * (pensionFund + healthcareIns + employmentContributions + injuryInsurance));
+    const grossDecreaseByContributions = grossSalary - totalContributions;
+    const personalRelief = 8400;
+    const taxBasisPersonalIncomeTax = grossDecreaseByContributions - (grossSalary > grossMin ? personalRelief : 0);
+    const personalIncomeTax = parseInt((taxBasisPersonalIncomeTax * 0.1).toFixed());
+    const totalContributionsAndTaxes = totalContributions + personalIncomeTax;
+
+ 
+    
+    useEffect(()=>{
+        setNetSalary(grossSalary - totalContributionsAndTaxes)
+        // eslint-disable-next-line
+    },[totalContributionsAndTaxes, grossSalary])
+
+    const changeGrossHendler = (e: ChangeEvent<HTMLInputElement>) =>  !e.target.value 
+    ? setGrossSalary(0) : setGrossSalary(parseInt( e.target.value))    
+     
+
+    const changeNetHendler = (e: ChangeEvent<HTMLInputElement>) => 
+    setNetSalary(parseInt(e.target.value) )
+     
+
+    
+    //console.log(grossSalary)  
     return (
         <div className="containerSalary">
             <div className="inputSlalary">
-                fsdsfssf
+                <div>
+                    <label>Gross Salary</label>
+                    <input type="number" value={grossSalary.toString()} onChange={changeGrossHendler}/>
+                    {
+                        grossSalary < grossMin && <p>Please insert amount bigger then 20335</p>
+                    }
+                </div>
+                <div>
+                    <label>Net Salary</label>
+                    <input type="number" value={netSalary.toString()} onChange={changeNetHendler}/>
+                    {
+                        netSalary < netMin && <p>Please insert amount bigger then 14000</p>
+                    }
+                </div>
             </div>
             <table className="tblSalary">
                 <tbody>
                 <tr>
-                    <td align="left">dddddddd</td>
-                    <td>0</td>
-                    <td>2222</td>
+                    <td align="left">Gross</td>
+                    <td></td>
+                    <td>{grossSalary}</td>
                 </tr>            
                 <tr>
-                    <td align="left">ddddddddddddddddd</td>
-                    <td>0</td>
-                    <td>2222</td>
+                    <td align="left">Pension fund</td>
+                    <td>18.80%</td>
+                    <td>{Math.round(grossSalary * pensionFund)}</td>
                 </tr>            
                 <tr>
-                    <td align="left">ddddddddddddddddd</td>
-                    <td>0</td>
-                    <td>2222</td>
+                    <td align="left">Healthcare Insurance</td>
+                    <td>7.50%</td>
+                    <td>{Math.round(grossSalary * healthcareIns)}</td>
                 </tr>            
                 <tr>
-                    <td align="left">ddddddddddddddddd</td>
-                    <td>0</td>
-                    <td>2222</td>
+                    <td align="left">Employment Contributions</td>
+                    <td>1.20%</td>
+                    <td>{Math.round(grossSalary * employmentContributions)}</td>
                 </tr>            
                 <tr>
-                    <td align="left">ddddddddddddddddd</td>
-                    <td>0</td>
-                    <td>2222</td>
+                    <td align="left">Insurance in case of injury</td>
+                    <td>0.50%</td>
+                    <td>{Math.round(grossSalary * injuryInsurance)}</td>
                 </tr>            
                 <tr>
-                    <td align="left">ddddddddddddddddd</td>
-                    <td>0</td>
-                    <td>2222</td>
+                    <td align="left">Total Contributions</td>
+                    <td></td>
+                    <td>{totalContributions}</td>
                 </tr>            
                 <tr>
-                    <td align="left">ddddddddddddddddd</td>
-                    <td>0</td>
-                    <td>2222</td>
+                    <td align="left">Gross Salary decrease by Contributions</td>
+                    <td></td>
+                    <td>{grossDecreaseByContributions}</td>
                 </tr>            
                 <tr>
-                    <td align="left">ddddddddddddddddd</td>
-                    <td>0</td>
-                    <td>2222</td>
+                    <td align="left">Personal Relief</td>
+                    <td></td>
+                    <td>8400</td>
                 </tr>            
                 <tr>
-                    <td align="left">ddddddddddddddddd</td>
-                    <td>0</td>
-                    <td>2222</td>
+                    <td align="left">Tax Basis for calculate Personal Income Tax</td>
+                    <td></td>
+                    <td>{taxBasisPersonalIncomeTax}</td>
                 </tr>            
                 <tr>
-                    <td align="left">ddddddddddddddddd</td>
-                    <td>0</td>
-                    <td>2222</td>
+                    <td align="left">Personal Income Tax</td>
+                    <td>10%</td>
+                    <td>{personalIncomeTax}</td>
                 </tr>            
                 <tr>
-                    <td align="left">ddddddddddddddddd</td>
-                    <td>0</td>
-                    <td>2222</td>
+                    <td align="left">Total Contributions and Taxes</td>
+                    <td></td>
+                    <td>{totalContributionsAndTaxes}</td>
                 </tr>            
                 <tr>
-                    <td align="left">ddddddddddddddddd</td>
-                    <td>0</td>
-                    <td>2222</td>
+                    <td align="left">Net Salary</td>
+                    <td></td>
+                <td>{netSalary}</td>
                 </tr>          
                 </tbody>                 
             </table>
